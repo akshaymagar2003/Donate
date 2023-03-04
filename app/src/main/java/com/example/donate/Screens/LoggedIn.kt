9e244c53bdcba2a11c1d66b05d3880e69b710f62
Akshay@2003 <akshay88magar@gmail.com>
2023-03-04 15:28:29 +0530

    Signup Signin Design Done

---
+++ app/src/main/java/com/example/donate/Screens/LoggedIn.kt
@@ -0,0 +1,12 @@
+package com.example.donate.Screens
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.donate.R
+
+class LoggedIn : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_logged_in)
+    }
+}
---
+++ app/src/main/java/com/example/donate/Screens/RegisterActivity.kt
@@ -0,0 +1,12 @@
+package com.example.donate.Screens
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.donate.R
+
+class RegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+    }
+}
