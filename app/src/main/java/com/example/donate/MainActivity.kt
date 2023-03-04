74f961cda45253fde85f7a894341f5049298633f
Akshay@2003 <akshay88magar@gmail.com>
2023-03-04 16:12:04 +0530

    Login Functioning Done

--- app/src/main/java/com/example/donate/MainActivity.kt
+++ app/src/main/java/com/example/donate/MainActivity.kt
@@ -1,11 +1,61 @@
-package com.example.donate
+package com.example.loginapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.example.donate.R
+import com.example.donate.Screens.LoggedIn
+import com.example.donate.Screens.RegisterActivity
+import com.example.donate.databinding.ActivityMainBinding
+import com.google.firebase.auth.FirebaseAuth
+
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var auth:FirebaseAuth
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+        auth= FirebaseAuth.getInstance()
+        binding.Register.setOnClickListener {
+            var intent =Intent(this, RegisterActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+
+
+        binding.Login.setOnClickListener {
+            if(checking()){
+                val email=binding.Email.text.toString()
+                val password= binding.Password.text.toString()
+                auth.signInWithEmailAndPassword(email, password)
+                    .addOnCompleteListener(this) { task ->
+                        if (task.isSuccessful) {
+                            var intent =Intent(this, LoggedIn::class.java)
+                            intent.putExtra("email",email)
+                            startActivity(intent)
+                            finish()
+                        } else {
+                            Toast.makeText(this, "Wrong Details", Toast.LENGTH_LONG).show()
+                        }
+                    }
+            }
+            else{
+                Toast.makeText(this,"Enter the Details",Toast.LENGTH_LONG).show()
+            }
+        }
+    }
+    private fun checking():Boolean
+    {
+        if(binding.Email.text.toString().trim{it<=' '}.isNotEmpty()
+            && binding.Password.text.toString().trim{it<=' '}.isNotEmpty())
+        {
+            return true
+        }
+        return false
     }
 }
